99e33b470b79a0f8971d91ed2eb7c1f7395901d9
harutas <96802323+harutas@users.noreply.github.com>
2022-11-08 20:53:50 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import FileUpload from "./components/FileUpload.vue";
+</script>
+
+<template lang="pug">
+FileUpload
+</template>
+
+<style scoped></style>
---
+++ src/components/FileUpload.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template lang="pug">
+div(class="h-screen flex flex-col justify-center")
+  div(class="text-center")
+    
+    input(type="file" class="file-input w-full max-w-xs border-slate-200")
+</template>
+
+<style scoped></style>
