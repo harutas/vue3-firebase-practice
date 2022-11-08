92d5909374777c945fff2c8c0f214735bd4fec49
harutas <96802323+harutas@users.noreply.github.com>
2022-11-08 23:37:27 +0900

    feat: login and logout

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -2,22 +2,23 @@
 import { storeToRefs } from "pinia";
 import useAuthStore from "../stores/auth";
 
-const { email, password } = storeToRefs(useAuthStore());
-const { registerUser } = useAuthStore();
+const { loginEmail, loginPassword } = storeToRefs(useAuthStore());
+const { logInUser } = useAuthStore();
 </script>
 
 <template lang="pug">
-div(class="flex flex-col items-center")
-  h2(class="text-3xl") ユーザ登録画面
-  form(@submit.prevent="registerUser")
+div(class="flex flex-col items-center mx-20")
+  h2(class="text-3xl") ログイン画面
+  form(@submit.prevent="logInUser")
     div(class="form-group")
-      label(for="email" class="label")
+      label(for="login-email" class="label")
         span(class="label-text") Email address
-      input(type="email" id="email" v-model="email" class="input input-bordered w-full max-w-xs" placeholder="john.doe@company.com" required)
+      input(type="email" id="login-email" v-model="loginEmail" class="input input-bordered w-full max-w-xs" placeholder="john.doe@company.com" required)
     div(class="form-group")
-      label(for="password" class="label")
+      label(for="login-password" class="label")
         span(class="label-text") Password
-      input(type="password" id="password" v-model="password" class="input input-bordered w-full max-w-xs" placeholder="" required)
-    div(class="text-center mt-2")
-      button(type="submit" class="btn btn-wide btn-info") 登録
+      input(type="password" id="login-password" v-model="loginPassword" class="input input-bordered w-full max-w-xs" placeholder="" required)
+    div(class="flex justify-between mt-2")
+      div(class="text-center")
+        button(type="submit" class="btn btn-wide btn-info") ログイン
 </template>
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,24 +1,56 @@
 import { defineStore } from "pinia";
 import { auth } from "../firebase/config";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 const useAuthStore = defineStore("auth", {
-  state: () => ({ isLoggedIn: true, email: "", password: "", repassword: "", errorMessage: "" }),
+  state: () => ({
+    isLoggedIn: false,
+    name: "",
+    email: "",
+    password: "",
+    loginEmail: "",
+    loginPassword: "",
+  }),
 
   actions: {
     // 新規ユーザー登録
     registerUser() {
-      console.log("firebase");
       // signin with email & password
       createUserWithEmailAndPassword(auth, this.email, this.password)
         .then((userCredential) => {
           // userの中にuidやdisplaynameなどがある
           const user = userCredential.user;
+          // ユーザーのプロファイルを更新する
+          updateProfile(user, {
+            displayName: this.name,
+            photoURL: "",
+          }).catch((error) => {
+            console.log(error.code);
+            console.log(error.message);
+          });
         })
         .catch((error) => {
           console.log(error.code);
           console.log(error.message);
         });
+    },
+    // 既存のユーザーにサインインする
+    logInUser() {
+      signInWithEmailAndPassword(auth, this.loginEmail, this.loginPassword)
+        .then((userCredential) => {
+          // SignIn
+          const user = userCredential.user;
+          this.loginEmail = "";
+          this.loginPassword = "";
+        })
+        .catch((error) => {
+          console.log(error.code);
+          console.log(error.message);
+        });
+    },
+    // ログアウトする
+    logOut() {
+      auth.signOut();
     },
   },
 });
